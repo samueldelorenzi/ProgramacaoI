bcfd5ba1f21ff37dd234874f90c270869ade0053
Samuel De Lorenzi Ribeiro <samueldelorenziribeiro@gmail.com>
2024-03-04 21:09:11 -0300

    Tipos numéricos

--- Exercícios/240219_01/Program.cs
+++ Exercícios/240219_01/Program.cs
@@ -43,3 +43,16 @@ bool isNomeEqual2 = string.Equals(nomeCompleto, "Samuel De Dalua Ribeiro");
 
 Console.WriteLine($"Primeiro valor: {isNomeEqual}");
 Console.WriteLine($"Segundo valor: {isNomeEqual2}");
+
+//tipos numericos
+/*
+    sbyte: armazena valores entre -128 e 127
+    short: armazena valores entre -32,768 e 32,767
+    int: armazena valores entre -2,147,483,648 e -2,147,483,647
+    long: armazena valores entre -9,223,372,036,854,775,808 e -9,223,372,036,854,775,807
+*/
+
+float myFloat = 10f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+int myInteger = 0;
