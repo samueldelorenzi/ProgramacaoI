8a2ea707dcdd0b5527d555c2a2a12b86b2950a66
Samuel De Lorenzi Ribeiro <samueldelorenziribeiro@gmail.com>
2024-03-17 21:05:36 -0300

    Tabuada um ao nove final

--- Atividades/TabuadaUmNove/Program.cs
+++ Atividades/TabuadaUmNove/Program.cs
@@ -4,97 +4,92 @@ class Program
 {
     static void Main()
     {
-        string operador;
+        string operador="";
         Console.WriteLine("Bem vindo a calculadora de tabuada");
-        Console.WriteLine("Escolha seu operador aritmético");
-        operador = Console.ReadLine() ?? "*";
+        while (operador != "+" & operador != "-" & operador != "*" & operador != "/")
+        {
+            Console.WriteLine("Escolha seu operador aritmético");
+            operador = Console.ReadLine() ?? "*";
+        }
         int resultado;
         int multiplicacao;
         string[,] array = new string[9,9];
 
-        bool contador = true;
-        while (contador == true)
+        switch (operador)
         {
-            switch (operador)
-            {
-                case "+":
-                    for (int i = 1; i < 10; i++)
+            case "+":
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int j = 1; j<10; j++)
                     {
-                        for (int j = 1; j<10; j++)
-                        {
-                            resultado=i+j;
-                            array[j-1,i-1] = $"{i} + {j} = {resultado}";
-                        }
+                        resultado=i+j;
+                        array[j-1,i-1] = $"{i} + {j} = {resultado}";
                     }
-                    for (int i = 0; i < 9; i++)
+                }
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
                     {
-                        for (int j = 0; j < 9; j++)
-                        {
-                            Console.Write($"{array[i,j]}\t"); 
-                        }
-                        Console.WriteLine("");
+                        Console.Write($"{array[i,j]}\t"); 
                     }
-                    contador = false;
-                break;
-                case "-":
-                    for (int i = 1; i < 10; i++)
+                    Console.WriteLine("");
+                }
+            break;
+            case "-":
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int j = 1; j<10; j++)
                     {
-                        for (int j = 1; j<10; j++)
-                        {
-                            resultado=i-j;
-                            array[j-1,i-1] = $"{i} - {j} = {resultado}";
-                        }
+                        resultado=i-j;
+                        array[j-1,i-1] = $"{i} - {j} = {resultado}";
                     }
-                    for (int i = 0; i < 9; i++)
+                }
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
                     {
-                        for (int j = 0; j < 9; j++)
-                        {
-                            Console.Write($"{array[i,j]}\t"); 
-                        }
-                        Console.WriteLine("");
+                        Console.Write($"{array[i,j]}\t"); 
                     }
-                    contador = false;
-                break;
-                case "*":
-                    for (int i = 1; i < 10; i++)
+                    Console.WriteLine("");
+                }
+            break;
+            case "*":
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int j = 1; j<10; j++)
                     {
-                        for (int j = 1; j<10; j++)
-                        {
-                            resultado=i*j;
-                            array[j-1,i-1] = $"{i} * {j} = {resultado}";
-                        }
+                        resultado=i*j;
+                        array[j-1,i-1] = $"{i} * {j} = {resultado}";
                     }
-                    for (int i = 0; i < 9; i++)
+                }
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
                     {
-                        for (int j = 0; j < 9; j++)
-                        {
-                            Console.Write($"{array[i,j]}\t"); 
-                        }
-                        Console.WriteLine("");
+                        Console.Write($"{array[i,j]}\t"); 
                     }
-                    contador = false;
-                break;
-                case "/":
-                    for (int i = 1; i < 10; i++)
+                    Console.WriteLine("");
+                }
+            break;
+            case "/":
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int j = 1; j<10; j++)
                     {
-                        for (int j = 1; j<10; j++)
-                        {
-                            multiplicacao=i*j;
-                            resultado=multiplicacao/i;
-                            array[j-1,i-1] = $"{multiplicacao} / {i} = {resultado}";
-                        }
+                        multiplicacao=i*j;
+                        resultado=multiplicacao/i;
+                        array[j-1,i-1] = $"{multiplicacao} / {i} = {resultado}";
                     }
-                    for (int i = 0; i < 9; i++)
+                }
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
                     {
-                        for (int j = 0; j < 9; j++)
-                        {
-                            Console.Write($"{array[i,j]}\t"); 
-                        }
-                        Console.WriteLine("");
+                        Console.Write($"{array[i,j]}\t"); 
                     }
-                    contador = false;
-                break;
-            }
+                    Console.WriteLine("");
+                }
+            break;
         }
     }
 }
