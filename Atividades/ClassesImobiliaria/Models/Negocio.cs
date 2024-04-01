45a6c5ceca2ef86b98bf0e157201a63697560ad7
Samuel De Lorenzi Ribeiro <samueldelorenziribeiro@gmail.com>
2024-04-01 20:02:57 -0300

    estrutura

---
+++ Atividades/ClassesImobiliaria/Models/Negocio.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassesImobiliaria.Models
+{
+    public class Negocio
+    {
+        public int Id { get; set; }
+        public string? negocio {get; set; }
+    }
+}
